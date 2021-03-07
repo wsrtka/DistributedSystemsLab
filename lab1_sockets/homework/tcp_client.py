a8ac17047b089591cb4e30648cd52c1de5c25e6b
wsrtka <witold.serwatka@pm.me>
2021-03-07 19:40:46 +0100

    added udp capability to client

--- lab1_sockets/homework/tcp_client.py
+++ lab1_sockets/homework/tcp_client.py
@@ -2,7 +2,10 @@ import socket
 import threading
 
 
-# funkcja odpowiadająca za odbieranie wiadomości
+server_ip = '127.0.0.1'
+server_port = 9008
+
+
 def receive(server):
     
     while True:
@@ -15,20 +18,31 @@ def receive(server):
             break
 
 
-# funkcja odpowiedzialna za wysyłanie wiadomości
 def write(nick, server):
+
+    # funkcja odpowiadająca za wysłanie wiadomości udp
+    def write_udp_msg():
+        
+        with open('heart.txt', 'r') as file:
+            udp_socket.sendto(file.read(), (server_ip, server_port))
+
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     while True:
         message = input(f'{nick}: ')
-        server.send(f'{nick}: {message}'.encode('utf-8'))
+
+        # reakcja na komendę użytkownika
+        if message == 'U':
+            write_udp_msg()
+        else: 
+            server.send(f'{nick}: {message}'.encode('utf-8'))
 
 
 if __name__ == '__main__':
 
-    # create_connection to funkcja ułatwiająca tworzenie klienta tcp, w pythonie 3.9 jest też funkcja ułatwiająca tworzenie serwera tcp
-    server = socket.create_connection(('127.0.0.1', 9008))
+    server = socket.create_connection((server_ip, server_port))
     msg = server.recv(1024).decode('utf-8')
 
-    # ustawianie nicku
     while msg == 'Enter nickname':
 
         print(msg)
@@ -36,7 +50,6 @@ if __name__ == '__main__':
         server.send(nick.encode('utf-8'))
         msg = server.recv(1024).decode('utf-8')
 
-    # wątki odpowiedzialne za wysyłanie i odbieranie wiadomości
     recv_thread = threading.Thread(target=receive, args=(server,))
     write_thread = threading.Thread(target=write, args=(nick, server))
 
