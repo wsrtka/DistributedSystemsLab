f67ba4dc8f840297701f201f10ccc1a5db4fb90b
wsrtka <witold.serwatka@pm.me>
2021-03-07 19:07:37 +0100

    minot changes for tcp client

--- lab1_sockets/homework/tcp_client.py
+++ lab1_sockets/homework/tcp_client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 
 
+# funkcja odpowiadająca za odbieranie wiadomości
 def receive(server):
     
     while True:
@@ -14,17 +15,20 @@ def receive(server):
             break
 
 
+# funkcja odpowiedzialna za wysyłanie wiadomości
 def write(nick, server):
     while True:
-        message = input()
+        message = input(f'{nick}: ')
         server.send(f'{nick}: {message}'.encode('utf-8'))
 
 
 if __name__ == '__main__':
 
+    # create_connection to funkcja ułatwiająca tworzenie klienta tcp, w pythonie 3.9 jest też funkcja ułatwiająca tworzenie serwera tcp
     server = socket.create_connection(('127.0.0.1', 9008))
     msg = server.recv(1024).decode('utf-8')
 
+    # ustawianie nicku
     while msg == 'Enter nickname':
 
         print(msg)
@@ -32,6 +36,7 @@ if __name__ == '__main__':
         server.send(nick.encode('utf-8'))
         msg = server.recv(1024).decode('utf-8')
 
+    # wątki odpowiedzialne za wysyłanie i odbieranie wiadomości
     recv_thread = threading.Thread(target=receive, args=(server,))
     write_thread = threading.Thread(target=write, args=(nick, server))
 
