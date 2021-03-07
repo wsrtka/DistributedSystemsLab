b0c9f54df45b460fe8ee3c7fd6a6389a684fc1ac
wsrtka <witold.serwatka@pm.me>
2021-03-07 19:05:06 +0100

    minor changes for tcp server

--- lab1_sockets/homework/tcp_server.py
+++ lab1_sockets/homework/tcp_server.py
@@ -2,17 +2,19 @@ import socket
 import threading
 
 
+# ustawienia serwera
 server_ip = '127.0.0.1'
 server_port = 9008
-
+# struktura do przechowywania nicków klientów wraz z ich połączeniami
 clients = {}
 
 
 # funkcja wysyłająca wiadomość do wszystkich podłączonych klientów
-def broadcast(message):
+def broadcast(message, sender):
     
     for client in clients.values():
-        client.send(message.encode('utf-8'))
+        if client != sender:
+            client.send(message.encode('utf-8'))
 
 
 # funkcja obsługująca wiadomość przychodzącą od klienta
@@ -23,11 +25,11 @@ def handle(nick):
     while True:
         try:
             msg = client.recv(1024).decode('utf-8')
-            broadcast(msg)
+            broadcast(msg, client)
         except:
             del clients[nick]
             client.close()
-            broadcast(f'{nick} left.')
+            broadcast(f'{nick} left.', client)
             print(f'Disconnected from {nick}')
             break
 
@@ -35,6 +37,7 @@ def handle(nick):
 # funkcja obsługująca nowe połączenie
 def receive():
     
+    # tworzone jest gniazdo serwera
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server_ip, server_port))
     server_socket.listen()
@@ -54,7 +57,7 @@ def receive():
                 break
 
         print(f'Set nick {nick}')
-        broadcast(f'{nick} joined.')
+        broadcast(f'{nick} joined.', None)
         client.send('Joined the server.'.encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(nick,))
