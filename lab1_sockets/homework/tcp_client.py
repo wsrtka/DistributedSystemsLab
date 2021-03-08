df2cfbf74cf934466bee810c291ad5fd87bf7157
wsrtka <witold.serwatka@pm.me>
2021-03-08 09:00:34 +0100

    add multicast function to client

--- lab1_sockets/homework/tcp_client.py
+++ lab1_sockets/homework/tcp_client.py
@@ -1,9 +1,13 @@
 import socket
 import threading
+from struct import pack
 
 
 server_ip = '127.0.0.1'
 server_port = 9008
+
+multicast_ip = '224.0.0.1'
+multicast_port = 9009
 
 
 def receive(server):
@@ -18,22 +22,33 @@ def receive(server):
             break
 
 
-def write(nick, server):
+# funkcja odbierająca wiadomości multicast
+def receive_multicast(mcast_socket):
 
-    # funkcja odpowiadająca za wysłanie wiadomości udp
-    def write_udp_msg():
+    while True:
+        try:
+            msg, _ = mcast_socket.recvfrom(1024)
+            print(msg.decode('utf-8'))
+        except:
+            print('An error occured')
+            mcast_socket.close()
+            break
 
-        with open('heart.txt', 'r') as file:
-            udp_socket.sendto(file.read().encode('utf-8'), (server_ip, server_port))
+
+def write(nick, server, mcast_socket):
 
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     while True:
         message = input(f'{nick}: ')
 
-        # reakcja na komendę użytkownika
         if message == 'U':
-            write_udp_msg()
+            with open('heart.txt', 'r') as file:
+            udp_socket.sendto(file.read().encode('utf-8'), (server_ip, server_port))
+        # obsługa wysłania wiadomości multicast
+        elif message == 'M'
+            with open('peace.txt', 'r') as file:
+                mcast_socket.sendto(file.read().encode('utf-8'), (multicast_ip, multicast_port))
         else: 
             server.send(f'{nick}: {message}'.encode('utf-8'))
 
@@ -50,8 +65,21 @@ if __name__ == '__main__':
         server.send(nick.encode('utf-8'))
         msg = server.recv(1024).decode('utf-8')
 
+    # tworzenie nowego gniazda multicast
+    mcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    mcast_socket.bind(('', multicast_port))
+
+    # dodanie gniazda do grupy multicast
+    group = socket.inet_aton(multicast_ip)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    mcast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
     recv_thread = threading.Thread(target=receive, args=(server,))
-    write_thread = threading.Thread(target=write, args=(nick, server))
+    write_thread = threading.Thread(target=write, args=(nick, server, mcast_socket))
+    # dodatkowy wątek na multicast
+    mcast_thread = threading.Thread(target=receive_multicast, args=(mcast_socket,))
 
     recv_thread.start()
     write_thread.start()
+    # rozpoczęcie działania wątku
+    mcast_thread.start()
