4e152bbd4d5e2fbf4fe5887aa2ce5ae8e5bf6cbc
wsrtka <witold.serwatka@pm.me>
2021-04-16 15:04:14 +0200

    add lab2 homework

---
+++ lab2_rabbitmq/Z1_Producer.java
@@ -0,0 +1,41 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Z1_Producer {
+
+    public static void main(String[] argv) throws Exception {
+
+        // info
+        System.out.println("Z1 PRODUCER");
+
+        // connection & channel
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // queue
+        String QUEUE_NAME = "queue1";
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);        
+
+        // producer (publish msg)
+        // String message = "Hello world!";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String message = br.readLine();
+
+        while(!message.equalsIgnoreCase("exit")){
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println("Sent: " + message);
+            message = br.readLine();
+        }
+
+        // close
+        channel.close();
+        connection.close();
+    }
+}
